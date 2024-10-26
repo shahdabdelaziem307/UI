aa4d55583465f0d901281c082fcc00dd3969bb71
Shahd abdelaziem <shahdabdelazem2003@gmail.com>
2024-10-27 00:47:15 +0300

    initial commit

---
+++ js/filr.js
@@ -0,0 +1,16 @@
+// var x = Number ( prompt ("enter your degree") )
+// if( x>50 && x<=60 ){
+//     alert ("passed")
+// }
+// else if( x>61 && x<=75 ){
+//     alert ("good")
+// }
+// else if( x>76 && x<=85 ){
+//     alert ("very good")
+// }
+// else if( x>86 && x<=100 ){
+//     alert ("excellent")
+// }
+// else {
+//     alert ("failed")
+// }
